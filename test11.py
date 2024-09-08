d32d340627ec26d6f3376669bd095e91d46c5b68
ckelly427 <cameron.kelly04@icloud.com>
2024-09-08 18:05:39 -0500

    a lot

--- test11.py
+++ test11.py
@@ -9,24 +9,46 @@ class studyApp:
         root.title("Study App")
         root.geometry("1000x600")
         root.resizable(width = False, height = False)
-        appFrame = tk.Frame(root,
-                            bg = '#ADD8E6')
-        appFrame.pack()
+        self.createFrame(self.root)
 
-        studyButton = tk.Button(root,
+    def createFrame(self, root):
+        self.appFrame = tk.Frame(root)
+        self.appFrame.pack()        
+
+        titleLabel = tk.Label(self.appFrame, 
+                            text = "Welcome to the study tool!",
+                            font = "impact 70",).pack()
+        
+        hr = tk.Canvas(self.appFrame, height=5, bg='white', bd=0, highlightthickness=0)
+        hr.pack(fill='x', pady=5)
+
+        studyButton = tk.Button(self.appFrame,
                             text = "Study a set",
-                            bg = '#ADD8E6',
-                            command = self.studyClicked(appFrame),
-                            width = "60",
-                            height = "10",
-                            activebackground = "blue",
-                            activeforeground = "blue")
+                            font = "impact 40",
+                            command = self.studyClicked,
+                            width = "12",
+                            height = "3")
+        
+        createButton = tk.Button(self.appFrame,
+                            text = "Create a set",
+                            font = "impact 40",
+                            command = self.studyClicked,
+                            width = "12",
+                            height = "3")
                             
         
-        studyButton.pack(side = "top")
+        studyButton.pack(side = "top", pady = "5")
+        createButton.pack(side = "top", pady = "5")
 
-    def studyClicked(self, frame):
-        pass
+    def studyClicked(self):
+        self.clearFrame()
+
+    def createClicked(self):
+        self.clearFrame()
+
+    def clearFrame(self):
+        for widget in self.appFrame.winfo_children():
+            widget.destroy()
 
 
 if __name__ == "__main__":
